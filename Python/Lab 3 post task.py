1597d78e62ece95516312deff260d11aa5d76aad
AbdulRahim <rahim381996@gmail.com>
2024-04-21 15:19:17 +0500

    lab 3 progress

--- Python/Lab 3 post task.py
+++ Python/Lab 3 post task.py
@@ -14,4 +14,63 @@ class person:
         print("Age of ", self.name, " is", age)
     
 obj = person("rahim", "Pakistan", "03/08/1996")
-print(obj.age())
+#print(obj.age())
+
+
+class Node:
+    left = None
+    right = None
+
+    def __init__(self, key):
+        self.key = key
+        
+
+class Tree:
+    
+    def __init__(self):
+        self.root = None
+
+    def insert(self, val):
+        self.root = self.insertRecursive(self.root, val)
+
+    def insertRecursive(self, root, val):
+        if(root is None):
+            return Node(val)
+        
+        if(val > root.key):
+            root.right = self.insertRecursive(root.right, val)
+        elif(val < root.key):
+            root.left = self.insertRecursive(root.left, val)
+
+        return root
+    def traversing(self):
+        self.traverse(self.root)
+
+    def traverse(self, root):
+        if root is not None:
+            self.traverse(root.left)
+            print(root.key)
+            self.traverse(root.right)
+
+    def search(self, val):
+        return self.insertRecursiveSearch(self.root, val)
+
+    def insertRecursiveSearch(self, parent, val):
+        if(parent is None):
+            return False
+        elif(parent.key == val):
+            return True
+        
+        if(val < parent.key):
+            return self.insertRecursiveSearch(parent.left, val)
+        elif(val > parent.key):
+            return self.insertRecursiveSearch(parent.right, val)
+
+tree = Tree()
+tree.insert(2)
+tree.insert(4)
+tree.insert(9)
+tree.insert(1)
+tree.insert(20)
+tree.traverse(tree.root)
+print(tree.search(2))
